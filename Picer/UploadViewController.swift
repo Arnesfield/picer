18401cb33cbc17ac8be89151b76e321abbcf90d8
Arnesfield <rylee.jeff385@gmail.com>
2017-11-19 20:28:48 +0800

    Added image picker

--- Picer/UploadViewController.swift
+++ Picer/UploadViewController.swift
@@ -8,13 +8,38 @@
 
 import UIKit
 
-class UploadViewController: UIViewController, UITextFieldDelegate {
+class UploadViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var txtField: UITextField!
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBAction func btnCamera(_ sender: UIButton) {
+        self.selectPhotoFrom(photoLibrary: false)
+    }
+    
+    @IBAction func btnGalery(_ sender: UIButton) {
+        self.selectPhotoFrom(photoLibrary: true)
+    }
+    
+    @IBAction func btnPost(_ sender: UIBarButtonItem) {
+    }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
+    }
+    
+    private func selectPhotoFrom(photoLibrary: Bool) -> Void {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = photoLibrary ? .photoLibrary : .camera
+        
+        self.present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        self.imgView.image = info[UIImagePickerControllerOriginalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
     }
     
     override func viewDidLoad() {
